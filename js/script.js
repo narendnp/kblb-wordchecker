a8296337970236933834f0aaa7d30bf8a7ec23de
narendnp <kadektop2@gmail.com>
2024-03-17 21:31:09 +0800

    fix not being able to check for multi-word phrases

--- js/script.js
+++ js/script.js
@@ -11,26 +11,36 @@ fetch('https://raw.githubusercontent.com/narendnp/kblb-banned-words/main/wordlis
     .then(response => response.text())
     .then(data => {
         wordList1 = data.split('\n'); // Assuming words are separated by newlines
+        checkButton.disabled = !wordList2;
     });
 
 fetch('https://raw.githubusercontent.com/narendnp/kblb-banned-words/main/wordlist-post.txt')
     .then(response => response.text())
     .then(data => {
         wordList2 = data.split('\n'); // Assuming words are separated by newlines
+        checkButton.disabled = !wordList1;
     });
 
 checkButton.addEventListener('click', () => {
-    const words = wordInput.value.split(' ');
+    const text = wordInput.value;
     const selectedWordList = wordListSelect.value === 'list1' ? wordList1 : wordList2;
-    const foundWords = words.filter(word => selectedWordList.includes(word));
+    
+    let foundWords = [];
+
+    selectedWordList.forEach(wordOrPhrase => {
+        if (text.includes(wordOrPhrase)) {
+            foundWords.push(wordOrPhrase);
+        }
+    });
+
     if (foundWords.length > 0) {
-        result.innerHTML = 'Kata yang tidak boleh digunakan: <strong>' + foundWords.join(', ') + '</strong>';
-        result.classList.add('alert-warning'); // Added warning class
+        result.innerHTML = 'Kata yang dilarang digunakan: <strong>' + foundWords.join(', ') + '</strong>.';
+        result.classList.add('alert-warning'); // Add warning class
         result.classList.remove('alert-success'); // Remove success class if it exists
     } else {
-        result.innerHTML = 'Teks tidak mengandung kata yang tidak boleh digunakan.';
-        result.classList.add('alert-success'); // Added success class
-        result.classList.remove('alert-warning'); // Remove danger class if it exists
+        result.innerHTML = 'Tidak terdapat kata yang dilarang digunakan.';
+        result.classList.add('alert-success'); // Add success class
+        result.classList.remove('alert-warning'); // Remove warning class if it exists
     }
 });
 
