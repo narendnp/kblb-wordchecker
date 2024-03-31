270867c391fc74e892d2fc5eda35de70f3ce044d
narendnp <kadektop2@gmail.com>
2024-03-31 14:04:14 +0700

    add typewriter fx to placeholder texts, add more placeholder texts, code cleanup

--- js/script.js
+++ js/script.js
@@ -3,27 +3,26 @@ const checkButton = document.getElementById('check-button');
 const result = document.getElementById('result');
 const wordListSelect = document.getElementById('word-list-select');
 
-let wordList1 = [];
-let wordList2 = [];
+let wordListComment = [];
+let wordListPost = [];
 
-// Fetch the word lists from the GitHub repo
 fetch('https://raw.githubusercontent.com/narendnp/kblb-banned-words/main/wordlist-comment.txt')
-    .then(response => response.text())
+    .then(response => response.text()) 
     .then(data => {
-        wordList1 = data.split('\n'); // Assuming words are separated by newlines
-        checkButton.disabled = !wordList2;
+        wordListComment = data.split('\n');
+        checkButton.disabled = !wordListPost;
     });
 
 fetch('https://raw.githubusercontent.com/narendnp/kblb-banned-words/main/wordlist-post.txt')
     .then(response => response.text())
     .then(data => {
-        wordList2 = data.split('\n'); // Assuming words are separated by newlines
-        checkButton.disabled = !wordList1;
+        wordListPost = data.split('\n');
+        checkButton.disabled = !wordListComment;
     });
 
 checkButton.addEventListener('click', () => {
     const text = wordInput.value;
-    const selectedWordList = wordListSelect.value === 'list1' ? wordList1 : wordList2;
+    const selectedWordList = wordListSelect.value === 'list1' ? wordListComment : wordListPost;
     
     let foundWords = [];
 
@@ -35,36 +34,69 @@ checkButton.addEventListener('click', () => {
 
     if (foundWords.length > 0) {
         result.innerHTML = 'Kata yang dilarang digunakan: <strong>' + foundWords.join(', ') + '</strong>.';
-        result.classList.add('alert-warning'); // Add warning class
-        result.classList.remove('alert-success'); // Remove success class if it exists
+        result.classList.add('alert-warning');
+        result.classList.remove('alert-success');
     } else {
         result.innerHTML = 'Tidak terdapat kata yang dilarang digunakan.';
-        result.classList.add('alert-success'); // Add success class
-        result.classList.remove('alert-warning'); // Remove warning class if it exists
+        result.classList.add('alert-success');
+        result.classList.remove('alert-warning');
     }
 });
 
-const placeholderTexts = [
+function updatePlaceholderText() {
+  const inputElement = document.getElementById("word-input");
+  const placeholderTexts = [
     "Jadi gini le...",
     "Stress le dikejer pinjol...",
     "Kalian lagi ngapain le...",
     "Kok belum tidur le...",
     "Gabut le...",
+    "Saran parfum budget under 200k le...",
+    "Le keluh kesah pake iphone gimana le...",
+    "Le tutor convert dosa ke saldo dana...",
+    "Umur segini temen udah pada nikah tapi gw masih segini2 aja le...",
+    "Le aku mau cerita barusan tante aku dateng ke rumah...",
+    "Gini amat le jadi anak, nasib dibanding2in sama tetangga...",
+    "Saran usaha modal dana 1jt le...",
     // Add more texts as needed
-];
+  ];
 
-function updatePlaceholderText() {
-   const inputElement = document.getElementById("word-input");
-   const randomIndex = Math.floor(Math.random() * placeholderTexts.length);
-    inputElement.placeholder = placeholderTexts[randomIndex];
+  let placeholderIndex = 0;
+  let previousIndex = -1; 
+
+  function typeWriter() {
+    if (placeholderIndex < placeholderTexts.length) {
+      let randomIndex;
+      do {
+        randomIndex = Math.floor(Math.random() * placeholderTexts.length);
+      } while (randomIndex === previousIndex); 
+      previousIndex = randomIndex;
+
+      const currentPlaceholderText = placeholderTexts[randomIndex];
+      let currentIndex = 0;
+
+      function typeCharacter() {
+        if (currentIndex < currentPlaceholderText.length) {
+          const currentPlaceholder = currentPlaceholderText.substring(0, currentIndex + 1);
+          inputElement.setAttribute("placeholder", currentPlaceholder);
+          currentIndex++;
+          setTimeout(typeCharacter, 100);
+        } else {
+          setTimeout(() => {
+            inputElement.setAttribute("placeholder", "");
+            placeholderIndex = 0;
+            typeWriter();
+          }, 1500); 
+        }
+      }
+
+      typeCharacter();
+    }
+  }
+
+  typeWriter();
 }
 
-const interval = 1500; // Keep the interval value
-let timeoutId;
-
-function frameLoop() {
+window.onload = function () {
   updatePlaceholderText();
-  timeoutId = setTimeout(frameLoop, interval);
-}
-
-timeoutId = setTimeout(frameLoop, interval);
+};
